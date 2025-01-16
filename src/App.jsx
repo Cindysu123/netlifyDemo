e427220022a15919b5be7ad0f3373a374050c07f
Cindysu123 <cindys@RISE09-PC.TrabusNet.com>
2025-01-16 00:58:43 -0800

    Removed Next.js plugin and updated build settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
-// App.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import './App.css';
+import Form from './Form'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      {/* Navigation Bar */}
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            My Website
-          </Typography>
-          <Box>
-            <Button color="inherit" component={Link} to="/">
-              Home
-            </Button>
-            <Button color="inherit" component={Link} to="/contact">
-              Contact
-            </Button>
-            <Button color="inherit" component={Link} to="/thank-you">
-              Thank You
-            </Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-
-      {/* Content of the Home page (App component) */}
-      <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the home page of the website!</p>
-      </div>
-    </div>
+    <Form />
   );
-};
-
+}
 export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,46 +1,30 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-
-export default function ContactForm() {
-    const navigate = useNavigate();
-
-    const handleSubmit = event => {
-        event.preventDefault();
-      
-        const myForm = event.target;
-        const formData = new FormData(myForm);
-      
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(formData).toString()
-        })
-          .then(() => navigate("/thank-you"))
-          .catch(error => alert(error));
-    };     
-
+export default function Form() {
     return (
-        <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit} netlify-honeypot="bot-field">
-            <input type="hidden" name="form-name" value="contact" />
-            <p>
-                <label>Your Name: <input type="text" name="name" /></label>
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-                <label>Your Role: <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-                </select></label>
-            </p>
-            <p>
-                <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-                <button type="submit">Send</button>
-            </p>
+        <form 
+        method='POST' 
+        name='contactform' 
+        className='contactForm'>
+
+        <input 
+            type='hidden'
+            name='form-name'
+            value='contactForm' />
+
+        <input 
+            type='text' 
+            name='name' 
+            placeholder='Enter your name' />
+
+        <input 
+            type='email' 
+            name='email' 
+            placeholder='Enter your email' />
+
+        <textarea 
+            name='message' 
+            placeholder='Messaage'></textarea>
+
+        <button type='submit'>Submit</button>
         </form>
     )
-
-}
+  }
